da131eb512fbecb5d07dbbb386b1668c57a5d9dc
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2019-10-27 14:19:38 +0200

    Fixed for clang 8

--- src/integration/AnalyzerPlugin.cpp
+++ src/integration/AnalyzerPlugin.cpp
@@ -1,11 +1,12 @@
 #include "SMACPPChecker.h"
 
-#include "clang/StaticAnalyzer/Core/CheckerRegistry.h"
+#include "clang/StaticAnalyzer/Frontend/CheckerRegistry.h"
 
 extern "C" void clang_registerCheckers(clang::ento::CheckerRegistry& registry)
 {
+    // TODO: the third parameter is docs url
     registry.addChecker<smacpp::SMACPPChecker>(
-        "smacpp.All", "Detects memory related errors with SMACPP");
+        "smacpp.All", "Detects memory related errors with SMACPP", "");
 }
 
 extern "C" const char clang_analyzerAPIVersionString[] = CLANG_ANALYZER_API_VERSION_STRING;
--- src/integration/ClangPlugin.cpp
+++ src/integration/ClangPlugin.cpp
@@ -2,7 +2,10 @@
 #include "parse/ClangASTAction.h"
 
 #include "clang/Frontend/FrontendPluginRegistry.h"
+#include "clang/StaticAnalyzer/Frontend/CheckerRegistry.h"
 // ------------------------------------ //
 
 static clang::FrontendPluginRegistry::Add<smacpp::ASTAction> X(
     "smacpp", "run smacpp static analysis");
+
+extern "C" const char clang_analyzerAPIVersionString[] = CLANG_ANALYZER_API_VERSION_STRING;
